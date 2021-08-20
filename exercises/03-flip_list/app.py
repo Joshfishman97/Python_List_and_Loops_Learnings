b1f6894c490c12b5ba5427f20cac26f983afc708
Joshfishman97 <fishmanjosh251@gmail.com>
2021-08-20 15:23:01 +0000

    another commit

--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,9 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
 
-#your code below:
+variable_new_list = []
+
+for i in range(len(arr)-1,-1,-1):
+    print(i)
+    variable_new_list.append(arr[i])
+
+    print(variable_new_list)
--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,7 +1,9 @@
-
+hello = []
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 
-#your code go here:
-
-print(hello)
+for i in range(0,len(my_list)):
+        if(type(dyict)==type(my_list[i]))
+        print(my_list[i])
+        #hello.append(my_list)
+        #print(hello)
